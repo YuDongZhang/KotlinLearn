f3ab9b25359a43f7ac70c1aef6d5f8e3ef115eae
YuDongZhang <977872239@qq.com>
2020-10-27 20:30:16 +0800

    添加fragment

---
+++ app/src/main/java/com/pdxx/kotlinlearn/SwitchFragmentActivity.kt
@@ -0,0 +1,11 @@
+package com.pdxx.kotlinlearn
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SwitchFragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_switch_fragment)
+    }
+}
