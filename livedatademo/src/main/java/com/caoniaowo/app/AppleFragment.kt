d9ec52c3b51960bf05cc824075f42e5880c97a31
张雨东 <zhangyudong@njpdxx.com>
2022-11-26 16:07:47 +0800

    完成了livedata 逻辑的实现

--- livedatademo/src/main/java/com/caoniaowo/app/AppleFragment.kt
+++ livedatademo/src/main/java/com/caoniaowo/app/AppleFragment.kt
@@ -7,6 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.map
+import androidx.lifecycle.switchMap
+import kotlinx.android.synthetic.main.fg_apple.*
 
 class AppleFragment : Fragment() {
 
@@ -21,6 +25,50 @@ class AppleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fg_apple, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        //在inActive状态下，是不会感知数据的，但是一旦resume，就会得到最新的数据
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as LiveActivity).apply {
+            liveAppleData.observe(viewLifecycleOwner, Observer {
+                tv_live_apple.text = it
+                Log.i("AppleFragment", "LiveData在AppleFragment中 $it")
+            })
+
+            //map换后的数值：
+            val liveMapApple = liveAppleData.map {
+                Log.d("AppleFragment", "LiveData在AppleFragment中 map $it")
+                "it mapped it ${it.takeLast(4)}"
+            }
+
+            liveMapApple.observe(viewLifecycleOwner, Observer {
+                val s = "mapped 后的值: $it"
+                tv_mapped_live_apple.setText(s)
+                Log.w("AppleFragment", "LiveData在AppleFragment中 map后的数据 $it")
+            })
+
+            //中介者MediatorLiveData使用演示：
+            mediatorLive.observe(viewLifecycleOwner, Observer {
+                //如果在inactive状态下，one two都变化了，它resume后只接受 代码顺序 最新添加的 source 的最后的值
+                val s = "mediator it $it"
+                tv_media_live_apple.setText(s)
+                Log.w("AppleFragment", "AppleFragment中 mediatorLive ---> $it")
+            })
+
+
+            //switch map 结合mediator，通过条件，控制选择数据源,这里模拟的是，it的数字奇偶，控制最终输出
+            val swLive = mediatorLive.switchMap {
+                if (it.second.takeLast(1).toInt() % 2 == 0) liveOne else liveTwo
+            }
+            //UI可以看出，不论是one，还是 two，改变的话，只有满足条件，才会生效。
+            swLive.observe(viewLifecycleOwner, Observer {
+                tv_switch_live_apple.text = it
+                Log.w("AppleFragment", "AppleFragment中 switchMap ---> $it")
+            })
+
+        }
     }
 
 
--- livedatademo/src/main/java/com/caoniaowo/app/LiveActivity.kt
+++ livedatademo/src/main/java/com/caoniaowo/app/LiveActivity.kt
@@ -2,12 +2,87 @@ package com.caoniaowo.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.map
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LiveActivity : AppCompatActivity() {
+    //声明变量live data
+    val liveAppleData = MutableLiveData<String>()
 
+    //中介者MediatorLiveData使用演示：
+    val liveOne = MutableLiveData<String>()
+    val liveTwo = MutableLiveData<String>()
+    val mediatorLive = MediatorLiveData<Pair<String, String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Fragment
+        val appleFragment = AppleFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fl_container_live, appleFragment)
+            .commit()
+        //// TODO 注意 hide 和 show不会改变fragment的生命周期状态 所以用attach detach
+        //显示fragment
+        btn_create_fg_live.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .attach(appleFragment)
+                .commit()
+            Log.d("LiveActivity", "onCreate: 显示fg ${appleFragment.isVisible}")
+        }
+        //隐藏
+        btn_destroy_fg_live.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .detach(appleFragment)
+                .commit()
+            Log.w("LiveActivity", "onCreate: 隐藏fg ${appleFragment.isVisible}")
+
+        }
+        //变更livedata的值
+        btn_change_live.setOnClickListener {
+            liveAppleData.value = "当前liveData的值：${System.currentTimeMillis()}"
+        }
+
+        //观察值
+        liveAppleData.observe(this, Observer {
+            tv_live_data_activity.text = it
+            Log.d("LiveActivity", "LiveData在LiveActivity中 $it")
+        })
+
+        //map转换后的数值：
+        val liveMappedData = liveAppleData.map {
+            Pair<Int, String>(it.hashCode(), it)
+        }
+
+        liveMappedData.observe(this, Observer {
+            tv_mapped_data_activity.text = "map 后的值：${it}"
+            Log.i("LiveActivity", "LiveData在LiveActivity中 map 后 $it")
+        })
+
+        //中介者MediatorLiveData使用演示：
+        btn_change1_live.setOnClickListener {
+            liveOne.value = "one:${System.currentTimeMillis().toString().takeLast(6)}"
+        }
+        btn_change2_live.setOnClickListener {
+            liveTwo.value = "two:${System.currentTimeMillis().toString().takeLast(6)}"
+        }
+
+        mediatorLive.addSource(liveTwo) {
+            Log.d("LiveActivity", "LiveActivity中 LiveTwo ---> $it")
+            mediatorLive.value = "two >>>>>" to it
+        }
+        mediatorLive.addSource(liveOne) {
+            Log.d("LiveActivity", "LiveActivity中 LiveOne ---> $it")
+            mediatorLive.value = "one >>" to it
+        }
+
+        mediatorLive.observe(this, Observer {
+            Log.w("LiveActivity", "LiveActivity中 mediatorLive ---> $it")
+        })
     }
 }
